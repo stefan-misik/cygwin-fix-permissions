27da6f2a69a12f73c8d1e229e10aa0eaed4b88e1
Stefan Misik <stefan-misik@users.noreply.github.com>
2019-04-03 13:37:10 +0200

    Added Microsoft Word documents

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -3,5 +3,5 @@
 find $1 -type f -regex ".*\.\("\
 "tex\|pdf\|mm\|tikz\|bst\|cls\|sty\|tcp\|bib\|m\|txt\|md\|freeplaneoptions"\
 "\|gitignore\|png\|jpg\|svg\|c\|h\|cpp\|hpp\|s\|inc\|ld\|mk\|xml\|readme"\
-"\|gpdsc\|project\|cproject"\
+"\|gpdsc\|project\|cproject\|doc\|docx"\
 "\)" -exec chmod 644 {} +
