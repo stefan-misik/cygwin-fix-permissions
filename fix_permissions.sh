0151c80f2f53624aa168eb648168d8aea21c7d3e
Stefan Misik <stefan-misik@users.noreply.github.com>
2019-09-05 11:08:11 +0200

    Add Excel file types

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -3,5 +3,5 @@
 find $1 -type f -regex ".*\.\("\
 "tex\|pdf\|mm\|tikz\|bst\|cls\|sty\|tcp\|bib\|m\|txt\|md\|freeplaneoptions"\
 "\|gitignore\|png\|jpg\|svg\|c\|h\|cpp\|hpp\|s\|inc\|ld\|mk\|xml\|readme"\
-"\|gpdsc\|project\|cproject\|doc\|docx"\
+"\|gpdsc\|project\|cproject\|doc\|docx\|xls\|xlsx\|csv"\
 "\)" -exec chmod 644 {} +
