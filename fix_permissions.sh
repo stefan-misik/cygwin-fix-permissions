fb54f029f80649ddfbe1c2b1d3b450ccd97821fa
Stefan Misik <stefan-misik@users.noreply.github.com>
2021-04-28 09:43:07 +0200

    Add QT user interface files

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -3,5 +3,5 @@
 find $1 -type f -regex ".*\.\("\
 "tex\|pdf\|mm\|tikz\|bst\|cls\|sty\|tcp\|bib\|m\|txt\|md\|freeplaneoptions"\
 "\|gitignore\|png\|jpg\|svg\|c\|h\|cpp\|hpp\|s\|inc\|ld\|mk\|xml\|readme"\
-"\|gpdsc\|project\|cproject\|doc\|docx\|xls\|xlsx\|csv"\
+"\|gpdsc\|project\|cproject\|doc\|docx\|xls\|xlsx\|csv\|ui"\
 "\)" -exec chmod 644 {} +
